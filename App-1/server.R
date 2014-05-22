149f161d65616efc80682c7b5bb3f0b169604410
BJB2088 <BJB2088@gmail.com>
2014-05-21 20:52:20 -0700

    x axes change

--- App-1/server.R
+++ App-1/server.R
@@ -3,7 +3,7 @@ shinyServer(function(input, output) {
   #sel <- reactive(quote({input$checkGroup}),quoted=TRUE)
   dat=reactive({subset(get2013dat,uniquecarrier %in% input$checkGroup)})
   output$txt1 = renderText({paste(input$checkGroup)})
-  output$plt1 = renderPlot(print(qplot(as.factor(month),sampleavg,geom="line",group=uniquecarrier,color=uniquecarrier,,data=dat())))
+  output$plt1 = renderPlot(print(qplot(as.factor(month),sampleavg,ylim=c(0,30),geom="line",group=uniquecarrier,color=uniquecarrier,data=dat())))
   })
 
 
